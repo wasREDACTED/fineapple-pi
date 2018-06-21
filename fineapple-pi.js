f508282db9b759586fe8f2e46fe5af9a06637cf9
wasREDACTED <greyson.yant@gmail.com>
2018-06-21 10:22:21 -0700

    added test read

--- fineapple-pi.js
+++ fineapple-pi.js
@@ -18,6 +18,12 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
+var ref = firebase.app().database().ref();
+ref.once(‘value’)
+ .then(function (snap) {
+ console.log(‘snap.val()’, snap.val());
+ });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
