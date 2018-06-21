b87376d240a1e27bfe0ead97f6ed7cb7a37a0a83
wasREDACTED <greyson.yant@gmail.com>
2018-06-21 11:42:13 -0700

    adding FB calls

--- fineapple-pi.js
+++ fineapple-pi.js
@@ -1,11 +1,22 @@
 var admin = require('firebase-admin');
-
 var serviceAccount = require('/home/pi/cred/i-entry-firebase-adminsdk-bpur2-5feda99169.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://i-entry.firebaseio.com'
 });
+var message = {text: 'testing pi-firebase', timestamp: new Date().toString()};
+var ref = firebase.database().ref().child('nodeclient');
+var logsRef = ref.child('messages');
+var messagesRef = ref.child('logs');
+var messageRef = messagesRef.push(message);
+
+logsRef.child(messageRef.key).set(message);
+
+logsRef.orderByKey().limitToLast(1).on('child_added', function(snap) {
+    console.log('added', snap.val());
+});
+logsRef.on()
 
 const http = require('http');
 
@@ -17,6 +28,8 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
+
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
