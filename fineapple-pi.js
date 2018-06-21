8bea21035377df855435d58fa734a5d96ec2a4a7
wasREDACTED <greyson.yant@gmail.com>
2018-06-21 15:52:51 -0700

    added lock

--- fineapple-pi.js
+++ fineapple-pi.js
@@ -7,6 +7,7 @@ admin.initializeApp({
 });
 var message = {text: 'testing pi-firebase', timestamp: new Date().toString()};
 var ref = admin.database().ref().child('nodeclient');
+var lock = admin.database().ref().child('lockStatus');
 var logsRef = ref.child('messages');
 var messagesRef = ref.child('logs');
 var messageRef = messagesRef.push(message);
@@ -14,7 +15,7 @@ var messageRef = messagesRef.push(message);
 logsRef.child(messageRef.key).set(message);
 
 logsRef.orderByKey().limitToLast(1).on('child_added', function(snap) {
-    console.log('added', snap.val());
+  console.log('added', snap.val());
 });
 logsRef.on('child_removed', function(snap) {
   console.log('removed', snap.val());
@@ -26,7 +27,7 @@ ref.child('logs').on('value', function(snap) {
   console.log('value', snap.val());
 });
 
-var lockStatus = admin.database().ref().child('lockStatus').on('value');
+var lockStatus = lock.on('value');
 
 const http = require('http');
 
