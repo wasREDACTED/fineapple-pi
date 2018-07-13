0baeaa0e28ce53fd448dcca95dbb3ac10e910012
wasREDACTED <greyson.yant@gmail.com>
2018-07-12 17:20:00 -0700

    added unlock timeout/firebase updates and "clean" exit

--- draft_fineapple-pi.js
+++ draft_fineapple-pi.js
@@ -1,6 +1,7 @@
 var admin = require('firebase-admin');
 var serviceAccount = require('./cred/i-entry-firebase-adminsdk-bpur2-5feda99169.json');
 var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out')
 
 //firebase initialization
 admin.initializeApp({
@@ -11,8 +12,6 @@ admin.initializeApp({
 //var message = {text: 'testing pi-firebase', timestamp: new Date().toString()};
 var ref = admin.database().ref().child('nodeclient');
 var lockRef = admin.database().ref().child('lockStatus');
-var logsRef = ref.child('messages');
-var messagesRef = ref.child('logs');
 //var messageRef = messagesRef.push(message);
 var lockValue = ""
 
@@ -33,37 +32,29 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-
-//--------RaspPi GPIO control section--------
-// Adds GPIO library, particularly onoff toggle
-var Gpio = require('onoff').Gpio;
-
-// associates LED to GPIO pin 4 "as out"
-var LED = new Gpio(4, 'out');
-
-// store blinkinterval as 
-var blinkInterval = setInterval(blinkLED, 100);
-
-return admin.database().ref().child('lockStatus').child('blinkTime').on('value', function(snap) {
-  var blinkTotaltime = snap.val();
-  console.log(`blinkTotaltime = ${blinkTotaltime}`)
+lockRef.child('Locked').on('value', function(snap) {
+  console.log('value', snap.val());
+  var lockState = snap.val();
+  if (lockState === 0) {
+    LED.writeSync(1);
+    console.log(lockState, 'Unlocked!');
+    setTimeout(function () {
+      if (lockState == 0) {
+          console.log('re-locking');
+          lockState = 1;
+          LED.writeSync(0);
+          lockRef.set({
+            Locked: "1"
+          })
+      }
+  }, 5000);
+  } else {
+    LED.writeSync(0);
+    console.log(lockState, 'DENIED');
+  }
 });
 
-function blinkLED() {
-	if (LED.readSync() === 0) {
-		LED.writeSync(1);
-	} else {
-		LED.writeSync(0);
-	}
-}
-
-function endBlink() {
-	clearInterval(blinkInterval);
-	LED.writeSync(0);
-	LED.unexport();
-}
-
-setTimeout(endBlink, 100);
-
-LED.writeSync(0);
-
+process.on('SIGINT', function () {
+  LED.unexport();
+  process.exit();
+});
